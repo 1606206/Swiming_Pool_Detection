fefcbde50d6ab77cc25f3eee28ad59db340ab767
Guillermo Vivancos Alonso <101990758+1606206@users.noreply.github.com>
2024-04-27 13:04:04 +0200

    structure yolov8

--- load_clasification.py
+++ load_clasification.py
@@ -2,6 +2,7 @@ import os
 import xml.etree.ElementTree as ET
 import numpy as np
 from keras.preprocessing.image import load_img, img_to_array
+import pandas as pd
 
 # Función para leer los archivos XML y obtener las etiquetas de las imágenes
 def parse_xml(xml_file):
@@ -40,16 +41,40 @@ def load_images_and_labels(img_dir, xml_dir):
                     images_without_pools.append(img_file)
     return images_with_pools, images_without_pools
 
-
+#guardamos un txt con los labels
+def write_label_file(image_filename, labels, output_dir, image_size=224):
+    txt_filename = os.path.join(output_dir, os.path.splitext(image_filename)[0] + '.txt')
+    with open(txt_filename, 'w') as f:
+        for bbox in labels:
+            # Calcula las coordenadas normalizadas
+            xmin_norm = bbox[0] / image_size
+            ymin_norm = bbox[1] / image_size
+            xmax_norm = bbox[2] / image_size
+            ymax_norm = bbox[3] / image_size
+            
+            f.write(f'0 {xmin_norm:.6f} {ymin_norm:.6f} {xmax_norm:.6f} {ymax_norm:.6f}\n')
 
 def clasification():
     img_dir = "DATABASE/training_data/images"
     xml_dir = "DATABASE/training_data/labels"
+    output_dir = os.path.join(img_dir, "pools_labels")  # Directorio de salida para los archivos .txt
+    
     images_with_pools, images_without_pools = load_images_and_labels(img_dir, xml_dir)
+    
+    # Crear el directorio de salida si no existe
+    os.makedirs(output_dir, exist_ok=True)
+    
     print("Imágenes con piscinas:")
     for img_file, labels in images_with_pools.items():
         print(img_file, ":", labels)
+        
+        # Escribir los datos de etiqueta en un archivo .txt en el directorio de salida
+        write_label_file(img_file, labels, output_dir)
 
     print("Imágenes sin piscinas:", images_without_pools)
     return images_with_pools, images_without_pools
+
+
+
+
 
